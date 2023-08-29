c91fa1b51d12f3f95bd92e7eca61f70fcb5ac9a6
Joseph Kariuki <joseph@hostkraft.cloud>
2023-08-29 03:55:03 +0300

    update config

--- src/config.js
+++ src/config.js
@@ -16,4 +16,13 @@ module.exports = {
   EMAILER_URL: process.env.EMAILER_URL || 'http://localhost:8082/emailer',
   API_URL:
     process.env.API_URL || 'http://localhost:8080',
+  DATA_DIRECTORY: process.env.DATA_DIRECTORY || path.join(root_dir, '/data'),
+  TEMPLATES_DIRECTORY:
+    process.env.TEMPLATES_DIRECTORY || path.join(root_dir, '/templates'),
+  TEMPORARY_DIRECTORY:
+    process.env.TEMPORARY_DIRECTORY || path.join(root_dir, '/tmp'),
+  PDF_DIRECTORY:
+    process.env.PDF_DIRECTORY || path.join(root_dir, '/pdf_documents'),
+  UPLOADS_DIRECTORY:
+    process.env.UPLOADS_DIRECTORY || path.join(root_dir, '/uploads'),
 };
